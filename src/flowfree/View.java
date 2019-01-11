6555b406a377b6d5d4171bbd672ede1068ca7f23
Tim <Timmy.severijns@gmail.com>
2019-01-11 18:55:16 +0100

    layout

--- src/flowfree/View.java
+++ src/flowfree/View.java
@@ -5,6 +5,7 @@
  */
 package flowfree;
 
+import java.util.ArrayList;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
@@ -18,6 +19,8 @@ import javafx.scene.shape.Rectangle;
  * @author GESE
  */
 public class View extends Pane{
+    private Color[] colors = {Color.BLUE, Color.RED, Color.GREEN, Color.YELLOW, Color.ORANGE};
+    
     public View () {
 
     }
@@ -28,7 +31,7 @@ public class View extends Pane{
         
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
-                Rectangle r = new Rectangle(x*51+5, y*51+5, 50, 50);
+                Rectangle r = new Rectangle(x*(500/size)+5, y*(500/size)+5,(500-size)/size, (500-size)/size);
                 r.setFill(Color.BLACK);
                 getChildren().add(r);
             }
@@ -36,10 +39,10 @@ public class View extends Pane{
         
         for (int i = 0; i < level.getCombinations().size(); i++) {
             Combination comb = level.getCombinations().get(i);
-            Circle c1 = new Circle(30+comb.getX1()*51, 30+comb.getY1()*51, 20);
-            c1.setFill(Color.RED);
-            Circle c2 = new Circle(30+comb.getX2()*51, 30+comb.getY2()*51, 20);
-            c2.setFill(Color.RED);
+            Circle c1 = new Circle(500/(2*size)+5+comb.getX1()*(500/size), 500/(2*size)+5+comb.getY1()*(500/size), (200/size));
+            c1.setFill(colors[i]);
+            Circle c2 = new Circle(500/(2*size)+5+comb.getX2()*(500/size), 500/(2*size)+5+comb.getY2()*(500/size),(200/size) );
+            c2.setFill(colors[i]);
             getChildren().addAll(c1, c2);
         }
         
